d2e71790a65ff28589e1d93cae4b639d1157c2b6
Renee Nordholm <reneenordholm@gmail.com>
2019-03-23 16:48:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,16 @@ def countdown(x)
 while x > 0
   puts "#{x} SECOND(S)!"
   x -= 1
- sleep(1.5)
+end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(y)
+  y = 10
+while y > 0
+  puts "#{x} SECOND(S)!"
+  y -= 1
+ sleep()
 end
   "HAPPY NEW YEAR!"
 end
